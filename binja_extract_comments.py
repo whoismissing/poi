586b55245a9258261cb1ff5fa7b16fa2d4e4a3f6
missing <whereismissing@gmail.com>
2022-03-01 18:31:46 -0500

    Added pydocs and user-provided request for saving

--- binja_extract_comments.py
+++ binja_extract_comments.py
@@ -1,9 +1,19 @@
 import time
 from pathlib import Path
+from typing import List
 
-from binaryninja.interaction import SaveFileNameField
+from binaryninja.interaction import (
+    get_form_input, 
+    SaveFileNameField
+)
 
-def binja_get_all_comments(bv):
+def binja_get_all_comments(bv) -> List[str]:
+    """
+    Return all the function comments from a given binary view in a special format.
+    :params bv: binaryninja.binaryview.BinaryView
+    :returns entries: list of function comments in the form of a string 
+                      0xaddress|function_prototype(int arg1, int arg2)|comment
+    """
     entries = []
     for fun in bv.functions:
         for addr, comment in fun.comments.items():
@@ -13,14 +23,25 @@ def binja_get_all_comments(bv):
     return entries
 
 def extract_comments(bv):
+    """
+    Ask the user for an outgoing filepath for saving the comments extracted from
+    a given binary view.
+    :params bv: binaryninja.binaryview.BinaryView
+    """
     comments = binja_get_all_comments(bv)
     
-    epoch = time.time()
-    bfilepath = Path(bv.file.filename)
-    out_file = str(epoch) + bfilepath.name + ".txt"
-    save_file = Path.cwd() / out_file
+    field = SaveFileNameField("Save file as")
+    get_form_input([field], "Outgoing file")
+    if field.result is None:
+        print("File save was cancelled!")
+        return
+    if field.result == "":
+        print("Empty filename provided!")
+        return
+    
+    save_file = Path(field.result)
     print("Extracting comments to", str(save_file))
     with open(str(save_file), "w") as fd:
         for comment in comments:
             fd.write(str(comment) + "\n")
-    print("Finished extracting")
+    print("Finished extracting.")
